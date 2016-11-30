4585613e4b040961a3feb4544c7e812d1db74be7
Wayne Zhang <wayne265265@gmail.com>
2016-11-30 16:29:46 +0800

    Fix cors

--- app.js
+++ app.js
@@ -3,20 +3,17 @@ var path = require('path');
 var fs = require('fs');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var http = require('http');
 var cors = require('cors');
-
 var app = express()
-
+var PORT = 3000;
 /* require Router setting */
 var issue = require('./routes/issue');
 var user = require('./routes/user');
 
-app.use('/issue', issue);
-app.use('/user', user);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var server = http.createServer(app).listen(PORT, function(){
+	console.log('HTTP Server listening on %s', PORT);
+});
 
 /* set router */
 app.use(cors());
@@ -24,20 +21,18 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,content-type, Accept');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
+
+app.use('/issue', issue);
+app.use('/user', user);

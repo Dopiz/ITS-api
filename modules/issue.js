0955619e5445f35a66255b8b5322216a6a7be0b1
WayneZhang <wayne265265@gmail.com>
2016-12-26 00:06:59 +0800

    Update addIssue

--- modules/issue.js
+++ modules/issue.js
@@ -1,13 +1,12 @@
 /* use database module */
 var database = require('./database');
-var multer = require('./multerUtil');
 var url = require('url');
 
 function getIssues(req, res){
     var tmp = url.parse(decodeURIComponent(req.url), true).search;
     if(tmp == null || tmp == ''){
         //var queryStatement = 'SELECT *, DATE_FORMAT(create_date, "%Y-%m-%d") AS createDate, DATE_FORMAT(due_date, "%Y-%m-%d") AS dueDate  FROM issue' ;
-        var queryStatement = 'SELECT id, project_name, priority, status, type, owner_id, developer_id, tester_id, DATE_FORMAT(create_date, "%Y-%m-%d") AS create_Date, DATE_FORMAT(due_date, "%Y-%m-%d") AS due_Date  FROM issue' ;
+        var queryStatement = 'SELECT id, project_id, priority, status, type, owner_id, developer_id, tester_id, DATE_FORMAT(create_date, "%Y-%m-%d") AS create_Date, DATE_FORMAT(due_date, "%Y-%m-%d") AS due_Date  FROM issue' ;
 
         database.query(queryStatement, function(error, results) {
 
@@ -67,45 +66,46 @@ function getIssuesByData(req, res, where){
     });
 }
 
-var issue_field = ['project name', 'title', 'priority', 'status', 'type', 'owner', 'developer', 'tester', 'create date', 'due date'];
+var issue_field = ['title', 'priority', 'status', 'type', 'owner', 'project id', 'developer', 'tester', 'create date', 'due date'];
 
 function addIssue(req, res){
-    var msg = 'Please enter a ';    
+    var msg = 'Please enter a ';
     var index = 0;  //記欄位index
     var count = 0;  //計算缺了幾個
 
     var data_set = {
-        project_name : req.body.project_name,
         title : req.body.title,
         priority : req.body.priority,
-        status : req.body.status,
+        status : (req.body.status) ? (req.body.status) : ("New"),
         type : req.body.type,
+        project_id : req.body.project_id,
         owner_id : req.body.owner_id,
         developer_id : req.body.developer_id,
         tester_id : req.body.tester_id,
+        description : req.body.description,
+        image : req.body.image,
         create_date : req.body.create_date,
         due_date : req.body.due_date
     };
 
-    for(x in data_set){        
-        if (data_set[x]==null || data_set[x]=='') {
-            if(count!=0)
-                msg += ', ';
-            msg += issue_field[index];
-            count++;
-        }
-        index++;
-    }
+    // for(x in data_set){
+    //     if (data_set[x]==null || data_set[x]=='') {
+    //         if(count!=0)
+    //             msg += ', ';
+    //         msg += issue_field[index];
+    //         count++;
+    //     }
+    //     index++;
+    // }
+    //
+    // if(count != 0){
+    //     res.send({
+    //         status_messages: msg,
+    //         status_code: 400
+    //     });
+    //     return;
+    // }
 
-    if(count != 0){
-        res.send({
-            status_messages: msg,
-            status_code: 400
-        });
-        return;
-    }
-
-    uploadImage(req,res);
 
     var queryStatement = 'INSERT INTO issue SET ?';
 
@@ -127,14 +127,6 @@ function addIssue(req, res){
     });
 }
 
-function uploadImage(req, res){
-    var upload = multer.single(req.file);
-    upload(req, res, function(err){
-        if(err){
-            return console.log(err);
-        }
-    })
-}
 
 module.exports.getIssues = getIssues;
 module.exports.addIssue = addIssue;
